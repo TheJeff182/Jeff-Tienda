e781b0acb111308084f12b111b7862a286a05932
Jeff <159392804+TheJeff182@users.noreply.github.com>
2025-11-22 20:38:24 -0600

    Agregando CRUDs de usuarios y funcionalidad de email

---
+++ src/main/java/tienda/repository/RolRepository.java
@@ -0,0 +1,16 @@
+package tienda.repository;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import tienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
